a6d2e10c32015bac095f214ca153d18c6f040a4a
lavanya-pg <lavanyapg8@gmail.com>
2021-06-29 15:49:11 +0530

    ADDED:edit a person details

---
+++ address/Contact.java
@@ -0,0 +1,22 @@
+package address;
+
+public class Contact
+{
+	String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    int zip;
+    long phoneNumber;
+    String email;
+	
+	
+	public String toString()
+	{
+		return "ContactItems [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
+				+ city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email
+				+ "]";
+	}
+
+}
